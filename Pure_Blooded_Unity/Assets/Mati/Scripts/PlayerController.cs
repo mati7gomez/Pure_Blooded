a12167dd0f30a8b1ab3e6ba74fca421b436a7153
mati7gomez <gomezmatiasagustin@gmail.com>
2024-04-11 23:43:05 -0300

    movimiento, linterna, camras, y rotacion anashex

--- Pure_Blooded_Unity/Assets/Mati/Scripts/PlayerController.cs
+++ Pure_Blooded_Unity/Assets/Mati/Scripts/PlayerController.cs
@@ -1,43 +1,101 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private CharacterController _controller;
-    Camera _mainCamera;
+    private CharacterController _controller;
+    private Camera _mainCamera;
+    private Transform _playerBody;
 
-    [SerializeField] private float _playerSpeed;
+    public bool CanMove = true;
 
     private float _horInput;
     private float _verInput;
-    Vector3 _camForward;
-    Vector3 _camRight;
+    [SerializeField] private float _playerSpeed;
+
+    private float _targetAngle;
+    private float _finalAngle;
+    private float _turnSmoothVelocity;
 
 
-
+    private Vector3 _camForward;
+    private Vector3 _camRight;
     private Vector3 _playerDir;
 
     private void Start()
     {
+        _playerBody = transform.GetChild(0).transform;
         _mainCamera = Camera.main;
         _controller = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        _camForward = Camera.main.transform.forward;
-        _camRight = Camera.main.transform.right;
+        Debug.Log(CanMove);
+        if (CanMove)
+            GetPlayerInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        GetCamVectors();
+        SetPlayerMoveDir();
+        MovePlayer();
+        RotatePlayer();
+
+        //Debug.Log(_controller.velocity.magnitude);
+    }
+    private void GetCamVectors()
+    {
+        _camForward = _mainCamera.transform.forward;
+        _camRight = _mainCamera.transform.right;
 
         _camForward.y = 0;
         _camRight.y = 0;
 
+        _camForward.Normalize();
+        _camRight.Normalize();
+    }
+    private void GetPlayerInputs()
+    {
         _horInput = Input.GetAxis("Horizontal");
         _verInput = Input.GetAxis("Vertical");
-
-        _playerDir = new Vector3(_horInput, 0f, _verInput);
-        _playerDir = Vector3.ClampMagnitude(_playerDir, 1f);
-
-        _controller.Move(_playerDir * _playerSpeed * Time.deltaTime);
     }
+    private void SetPlayerMoveDir()
+    {
+        _playerDir = _camForward * _verInput + _camRight * _horInput;
+
+        _playerDir = Vector3.ClampMagnitude(_playerDir, 1f);
+    }
+    private void MovePlayer()
+    {
+        _controller.Move(_playerDir * _playerSpeed * Time.fixedDeltaTime);
+    }
+    private void RotatePlayer()
+    {
+        if (_playerDir.magnitude != 0)
+        {
+            _targetAngle = Mathf.Atan2(_playerDir.x, _playerDir.z) * Mathf.Rad2Deg;
+            _finalAngle = Mathf.SmoothDampAngle(_playerBody.eulerAngles.y, _targetAngle, ref _turnSmoothVelocity, 0.1f);
+            _playerBody.rotation = Quaternion.Euler(0f, _finalAngle, 0f);
+        }
+        
+    }
+
+    public void DisableMovement()
+    {
+        CanMove = false;
+        _horInput = 0;
+        _verInput = 0;
+        _playerDir = Vector3.zero;
+        //Debug.Log("ayayaya q capo q soy");
+        Invoke("ActivateMovement", 0.1f);
+    }
+    private void ActivateMovement()
+    {
+        CanMove = true;
+    }
+
 }
