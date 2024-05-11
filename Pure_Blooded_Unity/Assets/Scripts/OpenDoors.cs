a7d50ed580ed3aab26d822dbae249a5fb0da7149
VitalGenesis <valenjueguitos2k23@gmail.com>
2024-05-11 04:15:58 -0300

    AbrirPuertas
    Logica para abrir puertas casi terminada, FALTA juntar parte del codigo con el inventario para restar la llave o sabor si existe la llave en el inventario.

---
+++ Pure_Blooded_Unity/Assets/Scripts/OpenDoors.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class AbrirPuertas : MonoBehaviour
+{
+    public Transform puerta;
+    public bool puedeAbrirPuerta = false;
+    public bool tengoLlave = false;
+
+    private void Start()
+    {
+        puerta = GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        AbrirPuerta();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Verifica si colisionó contra el jugador y presionó la tecla 'C'
+        if (other.gameObject.CompareTag("Player") && tengoLlave)
+        {
+            puedeAbrirPuerta = true;
+            Debug.Log("Puede Abrir la puerta");
+        }
+        else if (other.gameObject.CompareTag("Player") && !tengoLlave)
+        {
+            puedeAbrirPuerta = false;
+            Debug.Log("No puede Abrir la puerta, le falta una Llave");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !tengoLlave)
+        {
+            Debug.Log("Buena Suerte");
+            puedeAbrirPuerta = false;
+        }
+    }
+
+
+    // LOGICA PARA ABRIR LA PUERTA
+    private void AbrirPuerta()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && puedeAbrirPuerta)
+        {
+            // Girar la puerta -90 grados en el eje Y
+            puerta.Rotate(0f, -90f, 0f, Space.Self);
+
+            // Obtener la posición actual de la puerta
+            Vector3 posicionActual = puerta.position;
+
+            // Incrementar la posición actual en 1 en el eje X y 2 en el eje Z
+            posicionActual.x += 1f;
+            posicionActual.z += 2f;
+
+            // Establecer la nueva posición de la puerta
+            puerta.position = posicionActual;
+
+            Debug.Log("Puerta abierta");
+            // Quito la llave y por lo tanto el permiso para abrir la puerta
+            puedeAbrirPuerta = false;
+            tengoLlave = false;
+        }
+    }
+}
