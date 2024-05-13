d22cabec729ca071cd11033b9b9ae0367d87ed0e
VitalGenesis <valenjueguitos2k23@gmail.com>
2024-05-13 16:45:27 -0300

    Botones Pausa y Reanudar
    Falta implementar menú opciones y Menú opciones Inicial.

--- Pure_Blooded_Unity/Assets/Scripts/CanvasINICIOJUEGO/PortadaCanvas.cs
+++ Pure_Blooded_Unity/Assets/Scripts/CanvasINICIOJUEGO/PortadaCanvas.cs
@@ -2,13 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class PortadaCanvas : MonoBehaviour
 {
     // Hay que asignar este metodo al boton en la escena
+    public GameObject B_Pausa; 
+    public GameObject B_Despausa;
+    public GameObject FondoPanel;
+    private void Start()
+    {
+        Time.timeScale = 1f;
+        B_Pausa.SetActive(true);
+        B_Despausa.SetActive(false);
+        FondoPanel.SetActive(false);
+    }
+
     public void CambiarEscena()
     {
         // Cambiar a la escena deseada (asegúrate de tenerla en tu Build Settings)
         SceneManager.LoadScene(1);
     }
+
+    public void BotonPausa()
+    {
+        Time.timeScale = 0f;
+        B_Pausa.SetActive(false);
+        B_Despausa.SetActive(true);
+        FondoPanel.SetActive(true);
+    }
+    public void BotonReanudar()
+    {
+        Time.timeScale = 1f;
+        B_Pausa.SetActive(true);
+        B_Despausa.SetActive(false);
+        FondoPanel.SetActive(false);
+    }
+
 }
